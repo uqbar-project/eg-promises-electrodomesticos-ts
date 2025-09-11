27f475c242246e0a262212bce75d9e54d3eae962
fdodino <fernando.dodino@gmail.com>
2025-09-11 17:44:28 -0300

    add error handling

--- ejemplos_visualizer/40_fetch.js
+++ ejemplos_visualizer/40_fetch.js
@@ -1,5 +1,8 @@
 fetch('https://pokeapi.co/api/v2/ability/?offset=0&limit=20')
   .then((result) => {
+    if (!result.ok) {
+      throw new Error('Error en la llamada a la API: ' + result.statusText)
+    }
     return result.json()
   })
   .then((result) => {
