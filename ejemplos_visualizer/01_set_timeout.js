5ea8655d9ae9fe7fb5db3f67f159f7d3011edff7
fdodino <fernando.dodino@gmail.com>
2025-09-11 19:45:38 -0300

    Using npm run dev with default client

--- ejemplos_visualizer/01_set_timeout.js
+++ ejemplos_visualizer/01_set_timeout.js
@@ -1,6 +1,11 @@
+function imprimiteUn2() {
+  console.log(2)
+}
+
 function main() {
   console.log(1)
-  setTimeout(() => { console.log(2) }, 0)
+  setTimeout(() => { console.log(4) }, 150)
+  setTimeout(imprimiteUn2, 0)
   console.log(3)
 }
 
--- src/cliente.ts
+++ src/cliente.ts
@@ -35,16 +35,19 @@ export class Cliente {
 }
 
 // try {
-//   const cliente = new Cliente()
+const cliente = new Cliente(5000)
 // OK
-// .procesoDeCompra(new Electrodomestico('LCD TV', 4000), 3800)
+// cliente.procesoDeCompra(new Electrodomestico('LCD TV', 4000), 4200)
 // No puede volver en Taxi
-// .procesoDeCompra(new Electrodomestico('LCD TV', 4700), 4600)
+// cliente.procesoDeCompra(new Electrodomestico('LCD TV', 4700), 4800)
 // LCD TV -> no tengo plata
-// .procesoDeCompra(new Electrodomestico('LCD TV', 6000), 5100)
+// cliente.procesoDeCompra(new Electrodomestico('LCD TV', 6000), 6200)
 // LCD TV -> me la quieren vender muy cara
-//   cliente.procesoDeCompra(new Electrodomestico('LCD TV', 6000), 6100)
+cliente.procesoDeCompra(new Electrodomestico('LCD TV', 4000), 2000)
 //   console.log('Proceso de compra finalizado. Saldo: ' + cliente.saldo)
 // } catch(e) {
 //   console.log(e)
 // }
+
+console.info('Cliente queda con saldo de: ', cliente.saldo)
+console.info('Finalizado')
